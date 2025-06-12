9b3daeaa4a46ba9f9125e3080d7447a112d7af75
Daniel Pérez <dpdaniperezsanz@gmail.com>
2025-06-12 18:30:07 +0200

    Uncomment sentry import

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-// import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
