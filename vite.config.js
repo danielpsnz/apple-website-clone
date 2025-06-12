3757ba558f1bcbde426c50260f974cfbd0544b53
Daniel Pérez <dpdaniperezsanz@gmail.com>
2025-06-12 20:03:20 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/apple-website-clone/",
-  plugins: [
-    react(),
-  ],
-
-  build: {
-    sourcemap: true,
-  },
+  plugins: [react()],
 });
