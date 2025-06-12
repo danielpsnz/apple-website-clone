6bba44840095ce865e683ee3bac9d7343cb0c870
Daniel Pérez <dpdaniperezsanz@gmail.com>
2025-06-12 18:37:16 +0200

    Fix some bugs to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), 
+  base: "/apple-website-clone/",
+  plugins: [
+    react(),
     sentryVitePlugin({
-    org: "daniel-perez-ki",
-    project: "apple-website-clone"
-  })
+      org: "daniel-perez-ki",
+      project: "apple-website-clone",
+      include: "./dist",
+      telemetry: false,
+    }),
   ],
 
   build: {
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+  },
+});
