d2798c2caec330ea3db22c9853bc4a26751dfbbb
Daniel Pérez <dpdaniperezsanz@gmail.com>
2025-06-12 18:46:06 +0200

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -7,21 +7,6 @@ import * as Sentry from "@sentry/react";
 
 Sentry.init({
   dsn: "https://0d8c7f139fa028a92bb879ad7d31f042@o4509418344611840.ingest.de.sentry.io/4509486618181712",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.metrics.metricsAggregatorIntegration(),
-    Sentry.reactRouterV6BrowserTracingIntegration({
-      useEffect: React.useEffect,
-    }),
-    Sentry.replayIntegration({
-      maskAllText: false,
-      blockAllMedia: false,
-    }),
-  ],
-  tracesSampleRate: 1.0,
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
